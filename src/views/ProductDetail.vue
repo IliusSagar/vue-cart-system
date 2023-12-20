1d6bec296549c5de8ae0024d0e66626930f6d618
IliusSagar <sagar01830@gmail.com>
2023-12-20 23:57:09 +0600

    product details page fetch with api finnaly display

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Product Details</h1>
+  <div class="product">
+    <div class="product-image">
+      <img :src="selectedProduct.thumbnail" alt="" />
+    </div>
+    <div class="product-details">
+      <p>Brand: {{ selectedProduct.brand }}</p>
+      <p>Description: {{ selectedProduct.description }}</p>
+      <p>Price: {{ selectedProduct.price }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +18,27 @@ export default defineComponent({
   name: 'ProductDetails'
 })
 </script>
+
+<script setup>
+import { productsStore } from '@/stores/products'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const store = productsStore()
+const route = useRoute()
+
+const selectedProduct = computed(() => {
+  return store.products.find((item) => item.id === Number(route.params.id))
+})
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  margin-top: 50px;
+}
+
+.product-image {
+  margin-right: 24px;
+}
+</style>
